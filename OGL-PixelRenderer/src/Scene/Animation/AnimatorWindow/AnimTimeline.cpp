775fd969e1c6554479b81f548c3f63edf941fd0c
IvanNSBS <insbs@cin.ufpe.br>
2020-08-04 21:47:35 -0300

    Timeline line now has the correct size and position

--- OGL-PixelRenderer/src/Scene/Animation/AnimatorWindow/AnimTimeline.cpp
+++ OGL-PixelRenderer/src/Scene/Animation/AnimatorWindow/AnimTimeline.cpp
@@ -11,41 +11,50 @@ void AnimTimeline::Update()
 
 	else {
 		//Header
-		ImGui::BeginGroup(); 
-		{
-			ImGui::Text("FPS");
-			ImGui::SameLine();
-			ImGui::SetNextItemWidth(50);
-			ImGui::SliderInt("###fps", &_frameSampling, 0, 60);
-			ImGui::SameLine();
+		static float headerEndPos = 0;
 
-			ImGui::Text("Initial Frame");
-			ImGui::SameLine();
-			ImGui::SetNextItemWidth(50);
-			ImGui::SliderInt("###start", &_initialFrame, 0, _finalFrame);
-			ImGui::SameLine();
+		ImGui::BeginGroup(); {
 
-			ImGui::Text("Last Frame");
-			ImGui::SameLine();
-			ImGui::SetNextItemWidth(50);
-			ImGui::SliderInt("###end", &_finalFrame, _initialFrame, _maxFrames);
+			ImGui::BeginGroup(); 
+			{
+				ImGui::Text("FPS");
+				ImGui::SameLine();
+				ImGui::SetNextItemWidth(50);
+				ImGui::SliderInt("###fps", &_frameSampling, 0, 60);
+				ImGui::SameLine();
+
+				ImGui::Text("Initial Frame");
+				ImGui::SameLine();
+				ImGui::SetNextItemWidth(50);
+				ImGui::SliderInt("###start", &_initialFrame, 0, _finalFrame);
+				ImGui::SameLine();
+
+				ImGui::Text("Last Frame");
+				ImGui::SameLine();
+				ImGui::SetNextItemWidth(50);
+				ImGui::SliderInt("###end", &_finalFrame, _initialFrame, _maxFrames);
+				ImGui::EndGroup();
+			}
+
+			ImGui::BeginGroup();
+			{
+				ImGui::Text("Current Frame");
+				ImGui::SameLine();
+				ImGui::Text(std::to_string(_currentFrame).c_str());
+				ImGui::SameLine(0, 25); 
+				ImGui::Text("Current Time");
+				ImGui::SameLine();
+				ImGui::Text(std::to_string(_currentTime).c_str());
+				ImGui::EndGroup();
+			}
+
+			ImGui::Separator();
+			ImGui::EndGroup();
+			headerEndPos = ImGui::GetItemRectMax().y + 6;
 		}
-		ImGui::EndGroup();
-
-		ImGui::BeginGroup();
-		{
-			ImGui::Text("Current Frame");
-			ImGui::SameLine();
-			ImGui::Text(std::to_string(_currentFrame).c_str());
-			ImGui::SameLine(0, 25); 
-			ImGui::Text("Current Time");
-			ImGui::SameLine();
-			ImGui::Text(std::to_string(_currentTime).c_str());
-		}
-		ImGui::EndGroup();
 
 		//Timeline
-		ImGui::Separator();
+
 
 		ImDrawList* draw_list = ImGui::GetWindowDrawList();
 		ImGui::PushID("##HorizontalScrolling");
@@ -64,11 +73,18 @@ void AnimTimeline::Update()
 		ImGui::EndChild();
 		ImGui::PopID();
 
+
+		ImVec2 size = ImGui::GetItemRectSize();
+
 		ImVec2 window_pos = ImGui::GetWindowPos();
 		ImVec2 window_size = ImGui::GetWindowSize();
 		ImVec2 window_center = ImVec2(window_pos.x + window_size.x * 0.5f, window_pos.y + window_size.y * 0.5f);
-		ImVec2 top{ window_center.x + _finalFrame, window_center.y + window_size.y/2 };
-		ImVec2 bottom{ window_center.x + _finalFrame, window_center.y - window_size.y/2};
+
+		float offsetX = ((_finalFrame+1)/(float)(_maxFrames+1))*(window_size.x);
+
+		ImVec2 top{ window_pos.x + offsetX, headerEndPos };
+		ImVec2 bottom{ window_pos.x + offsetX, window_pos.y + window_size.y + size.y };
+
 		draw_list->AddLine(top, bottom, IM_COL32(255, 0, 0, 150), 1.5f);
 
 		End();
